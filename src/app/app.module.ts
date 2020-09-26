bac43bbd6af9cfb4c63c56c17271f5ee0e542163
Sang Mai <sangmai913@gmail.com>
2020-09-26 16:12:13 -0500

    Init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from "@angular/router";
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavMenuComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'about-us', component: AboutUsComponent },
+      { path: '**', pathMatch: 'full', redirectTo: '' },
+
+    ]),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/nav-menu/nav-menu.component.ts
@@ -0,0 +1,30 @@
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-menu',
+  templateUrl: './nav-menu.component.html',
+  styleUrls: ['./nav-menu.component.css']
+})
+export class NavMenuComponent {
+  isExpanded = false;
+  isScrolled = false;
+
+  collapse() {
+    this.isExpanded = false;
+  }
+
+  toggle() {
+    this.isExpanded = !this.isExpanded;
+  }
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    const number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (number > 70) {
+      this.isScrolled = true;
+    }
+    else {
+      this.isScrolled = false;
+    }
+  }
+}
